13003203870b462bd6243ee39fe5c6f2adf58df4
Weiming Hu <cosmos.weiming@outlook.com>
2016-08-06 04:54:14 +0800

    fix up django version diff

--- wsgi/myproject/trademap/models.py
+++ wsgi/myproject/trademap/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
+    # for extended User model usage, please refer to:
+    # http://stackoverflow.com/questions/6085025/django-user-profile
+
     user = models.OneToOneField(User, related_name='profile')
     phone = models.CharField(max_length=12)
     home_lat = models.DecimalField(max_digits=19, decimal_places=10, null=True)
